c6d3bfb98bc3732671d9c04cd00c65a606d7c756
gege86 <nagy.gergely1888@gmail.com>
2020-10-08 10:37:52 +0200

    Update 023_top_earners.sql

--- 023_top_earners.sql
+++ 023_top_earners.sql
@@ -1,12 +1,17 @@
--- wip
 select name, salary * months from employee ;
 select name, salary * months from employee order by 2 desc ;
 -- we can see from the above output that 7 ppl have the same max (just to check)
 select max(salary * months) from employee ;
-select max(salary * months), count(id)
+select count(employee_id) from employee where salary * months = 108064 ;
+select max(salary * months), count(employee_id)
   from employee
   where
     (salary * months) = max(salary * months)  
 ;
 -- ??
-
+-- checked the discussion page and realized that the above "where" was missing a "select"
+select max(salary * months), count(employee_id)
+  from employee
+  where
+    (salary * months) = (select max(salary * months) from employee) 
+;
