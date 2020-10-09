f3cd30c9490a7f1073d0f155a79a62e6e82762c9
gege86 <nagy.gergely1888@gmail.com>
2020-10-09 23:06:58 +0200

    Update 030_weather_station_18.sql

--- 030_weather_station_18.sql
+++ 030_weather_station_18.sql
@@ -1,1 +1,10 @@
--- wip
+select
+    dec(
+        round(
+            abs(min(lat_n) - max(lat_n)) +
+            abs(min(long_w) - max(long_w))
+            ,4),
+        15,4)
+from station
+;
+-- ez pz!!! : )
