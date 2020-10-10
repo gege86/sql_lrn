0f6941a196581f4de89f03afef51f594e778d58e
gege86 <nagy.gergely1888@gmail.com>
2020-10-10 23:09:44 +0200

    Update 034_the_pads.sql

--- 034_the_pads.sql
+++ 034_the_pads.sql
@@ -1,2 +1,12 @@
+-- first query
 select name || '(' || left(occupation,1) || ')' from occupations order by name ;
-select 'There are a total of ' || count(occupation) || ' ' || lcase(occupation) || 's.' from occupations group by occupation order by count(occupation), occupation ;
+-- second query
+select 'There are a total of ' || 
+  count(occupation) || 
+  ' ' || 
+  lcase(occupation) || 
+  's.' 
+from occupations
+group by occupation
+order by count(occupation), occupation
+;
