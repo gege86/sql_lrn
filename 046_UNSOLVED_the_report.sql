f8902c329ca697f32d8ee9b0603c5559575da42b
gege86 <nagy.gergely1888@gmail.com>
2020-10-15 21:18:08 +0200

    one more

--- 046_UNSOLVED_the_report.sql
+++ 046_UNSOLVED_the_report.sql
@@ -1,1 +1,36 @@
 --wip
+-- initial join
+select 
+	*
+from
+    students
+    full outer join grades
+    on students.id = grades.grade
+;
+--
+select 
+	students.name,
+	grades.grade,
+	students.marks
+from
+    students
+    full outer join grades
+    on students.id = grades.grade
+;
+--
+select 
+    case 
+        when (select grades.grade from grades where students.marks between grades.min_mark and grades.max_mark) >= 8 then students.name
+        else 'NULL'
+    end,
+    (select grades.grade from grades where students.marks between grades.min_mark and grades.max_mark),
+    students.marks
+from
+    students
+    full outer join grades
+    on students.id = grades.grade
+where
+    students.name is not null
+order by 2 desc, 1 asc, 3 asc
+;
+-- there must be a better way, need to check other solutions
